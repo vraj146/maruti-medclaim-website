fda180f2b32a66f74a8595fbcff8f9c998d13fb7
vraj <vrajpatel8624@gmail.com>
2025-07-27 20:36:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("responseMessage").textContent = "Thank you! We will contact you shortly.";
+  this.reset();
+});
+function toggleMenu() {
+  document.getElementById("navbar").classList.toggle("show");
+}
